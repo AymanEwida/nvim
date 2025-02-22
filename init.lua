a990af055d2569ee3965ebfc555842b04072f12e
AymanEwida <ayman25a.05@gmail.com>
2025-02-23 01:08:06 +0200

    feat: Telescope and Treesetter

--- init.lua
+++ init.lua
@@ -4,21 +4,27 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+
     if vim.v.shell_error ~= 0 then
       vim.api.nvim_echo({
         { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
         { out, "WarningMsg" },
         { "\nPress any key to exit..." },
       }, true, {})
+
      vim.fn.getchar()
      os.exit(1)
    end
+
  end
 
 vim.opt.rtp:prepend(lazypath)
@@ -26,9 +32,32 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    
+    {
+      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
   },
   checker = { enable = true },
 })
 
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Treesitter
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "lua", "javascript" },
+  highlight = { enable = true },
+  indent = { enable = true }, 
+})
+
+-- ColorScheme
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
