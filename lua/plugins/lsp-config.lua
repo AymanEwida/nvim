99a334d2acb487044dcf3a8bb2a5daacd4a89f7b
AymanEwida <ayman25a.05@gmail.com>
2025-10-12 13:31:47 +0300

    feat: go formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -55,6 +55,13 @@ return {
                 capabilities = capabilities,
             })
 
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.go",
+                callback = function()
+                    vim.lsp.buf.format({ async = false })
+                end,
+            })
+
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
